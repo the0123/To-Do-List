b4bdda6005c31acb695174bc81410925dbad5d70
the0123 <amdetheodros@gmail.com>
2023-12-27 14:57:30 -0500

    Added option to reset user input and option to delete all tasks

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-document.getElementById('submitButton').onclick = function(){
-    let text = document.getElementById('taskInput').value;     
+document.getElementById("submitButton").onclick = function(){
+    let text = document.getElementById("taskInput").value;
     let node = document.createElement("li");    
     let textNode = document.createTextNode(text);    
     node.appendChild(textNode);     
-    document.getElementById('taskList').appendChild(node);
+    document.getElementById("taskList").appendChild(node);
   };
 
+  document.getElementById("deleteAll").onclick = function() {
+    let ul = document.getElementById("taskList");
+    if(ul) {
+      while(ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+      }
+    }
+  };
+ 
